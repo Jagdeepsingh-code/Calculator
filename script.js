89d48ce75d306eb0403199fb957201b9d718d6f3
Jagdeepsingh-code <jagdeepsaggu75@gmail.com>
2024-07-28 17:25:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let var1, var2, op;
+let result = document.querySelector(".result>h2");
+let ac = document.querySelector("#ac");
+
+ac.addEventListener("click", () => {
+  console.log("ac clicked");
+  result.innerHTML = "WLCM TO MYCALC";
+});
